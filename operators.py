6db7cbeae2fed86139f5702b5442fa575ece59b0
chaudharyriya2011-ship-it <chaudharyriya2011@gmail.com>
2026-09-24 21:22:27 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,16 @@
+a = 5
+b = 10
+c = 15
+if a and b and c:
+    print("true")
+else:
+    print("one value is flase")
+a = 10
+b = -10
+c = 20
+if a>0 or b>0:
+    print("Either is greater than zero")
+else:
+    print("no number is zero")
+if b>0 or c>0:
+    print("Either is greater than zero")
