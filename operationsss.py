e1b8f4c8a1db467e310e8a14098eaba1023a5167
chaudharyriya2011-ship-it <chaudharyriya2011@gmail.com>
2026-09-24 21:37:54 +0530

    operation

---
+++ operationsss.py
@@ -0,0 +1,16 @@
+a = 10
+b = 15
+c = 20
+print(a!=b)
+print(not(b==c))
+a = "python"
+b = "coding"
+if not (a==b):
+    print("they are different")
+a = 1
+b = 1
+if not ((a==1)==(b==5)):
+    print("hello")
+a = int(input("Enter a number "))
+if not (a%2==0):
+    print("odd number")
